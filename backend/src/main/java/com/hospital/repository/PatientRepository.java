68d4fe7f51b1663e86cd3ba4adf5613a77ae316a
yue <1558773090@qq.com>
2025-11-22 16:42:06 +0800

    Update PatientRepository.java

--- backend/src/main/java/com/hospital/repository/PatientRepository.java
+++ backend/src/main/java/com/hospital/repository/PatientRepository.java
@@ -1,12 +1,14 @@
 package com.hospital.repository;
 
 import com.hospital.model.Patient;
-import com.hospital.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
-    List<Patient> findByOwner(User owner);
-}
 
+    /**
+     * 查询某个用户的全部就诊人
+     */
+    List<Patient> findByUserId(Long userId);
+}
