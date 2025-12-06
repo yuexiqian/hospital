65c26d7bf5a1291fc5987ede9e2fe537da260a83
yue <1558773090@qq.com>
2025-12-06 16:12:23 +0800

    Update DeptService.java

--- backend/src/main/java/com/hospital/service/DeptService.java
+++ backend/src/main/java/com/hospital/service/DeptService.java
@@ -1,10 +1,14 @@
 package com.hospital.service;
 
 import com.hospital.model.Dept;
+import com.hospital.dto.DeptDoctorOverviewDTO;
+
+import java.time.LocalDate;
 
 import java.util.List;
 
 public interface DeptService {
 
     List<Dept> listAll();
+    DeptDoctorOverviewDTO getDeptOverviewWithDoctors(Long deptId, LocalDate date);
 }
